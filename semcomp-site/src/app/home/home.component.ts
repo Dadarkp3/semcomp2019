721a64cecdf752cd19c276f380968cc57800e671
Daiane <adm.daianesilva@gmail.com>
2019-03-23 20:47:13 -0300

    Adição de contador

--- semcomp-site/src/app/home/home.component.ts
+++ semcomp-site/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.sass"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  valor: number;
+  constructor() {}
 
   ngOnInit() {
+    this.valor = 0;
   }
 
+  public adicionar(): void {
+    this.valor++;
+  }
+
+  public diminuir(): void {
+    this.valor--;
+  }
 }
